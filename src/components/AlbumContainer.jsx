3246d3808c2b67d6b2e320c30a3d9f315a440e0f
gabriel <franklingabriel158@gmail.com>
2022-04-12 15:58:12 -0300

    alteração no componente AlbumContainer que adiciona imagem

--- src/components/AlbumContainer.jsx
+++ src/components/AlbumContainer.jsx
@@ -6,10 +6,14 @@ class AlbumContainer extends Component {
   render() {
     const { artist, albuns } = this.props;
     const albumCards = albuns.map((album) => (
-      <AlbumCard
-        key={ album.collectionId }
-        album={ album }
-      />
+      <><><AlbumCard
+        key={album.collectionId}
+        album={album} /><div>
+          <img src={album.artworkUrl100} alt={album.collectionName} />
+        </div></><div>
+          <h5>{album.collectionName}</h5>
+          <p>{album.artist}</p>
+        </div></>
     ));
     return (
       <section>
